f147e5ca5a737b03917fae5b776fa54bfa4f30b7
alvarado0925 <alvarado0925@gmail.com>
2024-05-29 22:47:59 -0500

    First commit - Imprimir hello world

---
+++ 01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+  //var myName = 'Felipe';
+  String myName = 'Felipe';
+
+  // Si se quiere imprimir una variable dentro de una cadena de texto
+  print('hola $myName');
+
+  // Si se quiere hacer operaciones dentro la cadena
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${4 + 5}');
+}
