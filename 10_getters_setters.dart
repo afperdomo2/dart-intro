8b5b3a9ca2f7ef1a86619e10e183e6ce45500339
alvarado0925 <alvarado0925@gmail.com>
2024-06-10 00:07:20 -0500

    Usando getters y setters en las clases

---
+++ 10_getters_setters.dart
@@ -0,0 +1,29 @@
+void main() {
+  final mySquare = Square(side: 10);
+
+  mySquare.side = -15;
+
+  print('Area: ${mySquare.area}');
+}
+
+class Square {
+  double _side;
+
+  Square({required double side}) : _side = side;
+
+  set side(double value) {
+    print('Setting new value: ${value}');
+    if (value < 0) {
+      throw "Value must be >= 0";
+    }
+    _side = value;
+  }
+
+  double get area {
+    return _side * _side;
+  }
+
+  double calculateArea() {
+    return _side * _side;
+  }
+}
