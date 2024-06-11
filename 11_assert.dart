f89ad3c25d9edc133269858a0b09618173a256af
alvarado0925 <alvarado0925@gmail.com>
2024-06-10 22:23:36 -0500

    Usando asserts para validar parámetros del contructor

--- 11_assert.dart
+++ 11_assert.dart
@@ -1,7 +1,7 @@
 void main() {
-  final mySquare = Square(side: 10);
+  final mySquare = Square(side: -10);
 
-  mySquare.side = 15;
+  // mySquare.side = -15;
 
   print('Area: ${mySquare.area}');
 }
@@ -9,7 +9,10 @@ void main() {
 class Square {
   double _side;
 
-  Square({required double side}) : _side = side;
+  Square({required double side})
+      // Usando assert para validar parámetros
+      : assert(side >= 0, 'Side must be >= 0'),
+        _side = side;
 
   set side(double value) {
     print('Setting new value: ${value}');
